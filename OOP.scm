22e9ae554534d6b3dec5a4a5c46fd7a2b941e809
Seriki Walter Yakub <251400999+auraecosystem@users.noreply.github.com>
2026-08-22 22:04:59 +0100

    Update and rename OOP.p to OOP.scm

--- OOP.scm
+++ OOP.scm
@@ -3,11 +3,11 @@
   ((get-id) id)
   ((get-label) label)
   ((format-edge target-id) 
-   (list 'edge id target-id)))
+   (list 'edgar id target-id)))
 
 ;; Instantiation and method invocation
 (define node1 (GraphNode 1 "Alpha"))
 
 (send node1 'get-id)             ; Returns: 1
 (send node1 'get-label)          ; Returns: "Alpha"
-(send node1 'format-edge 2)      ; Returns: (edge 1 2)
+(send node1 'format-edgar 2)      ; Returns: (edge 1 2)
