8e75390ccb983101fc99a6786e9d3bbefe9dc1f3
Seriki Walter Yakub <251400999+auraecosystem@users.noreply.github.com>
2026-08-29 03:02:20 +0100

    Update and rename kubu-hai.model.h5 to Lmlm/kubu-hai.model.h5/Pkg.pyx

--- Lmlm/kubu-hai.model.h5/Pkg.pyx
+++ Lmlm/kubu-hai.model.h5/Pkg.pyx
@@ -15,7 +15,7 @@ def setup_logging(log_level=logging.INFO):
 logging.basicConfig(level=log_level, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 return logger
-
+ 
 logger = setup_logging()
 logger.info("Kubu-Hai package initialized")
 
