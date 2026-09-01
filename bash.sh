6fc80ef1e04a025ca7a70b3415fad70c60abb4a1
Seriki Walter Yakub <251400999+auraecosystem@users.noreply.github.com>
2026-09-01 13:51:15 +0100

    Enhance bash script with service instructions
    Added instructions for running backend and frontend services, including usage of codemod scripts.

--- bash.sh
+++ bash.sh
@@ -51,3 +51,41 @@ EOF
 git add .gitattributes
 git commit -m "fix: configure .gitattributes to fix language stats"
 git push origin main
+
+man git-clone https://github.com/josStorer/RWKV-Runner
+
+# Then
+cd RWKV-Runner
+python ./backend-python/main.py #The backend inference service has been started, request /switch-model API to load the model, refer to the API documentation: http://127.0.0.1:8000/docs
+
+# Or
+cd RWKV-Runner/frontend
+npm ci
+npm run build #Compile the frontend
+cd ..
+python ./backend-python/webui_server.py #Start the frontend service separately
+# Or
+python ./backend-python/main.py --webui #Start the frontend and backend service at the same time
+
+# Help Info
+python ./backend-python/main.py -h
+git clone https://github.com/auraecosystem/jssg
+cd jssg
+zig build
+npx codemod @nodejs/cjs-to-esm
+
+This repository contains codemods (automated migrations) for "userland" code. These are intended to facilitate adopting new features and upgrading source-code affected by breaking changes.
+
+## Usage
+
+> [!CAUTION]
+> These scripts change source code. Commit any unsaved changes before running them. Failing to do so may ruin your day.
+
+To run the transform scripts use [`codemod`](https://go.codemod.com/github) command below:
+
+### From registry
+
+With the codemod CLI you can run a workflow from the [Codemod Registry](https://codemod.link/nodejs-official). Replace `<recipe>` with the name of the recipe you want to run:
+
+```bash
+npx codemod @nodejs/<recipe>
